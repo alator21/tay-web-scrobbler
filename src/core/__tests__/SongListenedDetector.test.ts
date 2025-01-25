32ae70c8edef33b02ee102ec3a0eaac68d2978a5
alator21 <botazz133@gmail.com>
2025-01-25 15:24:23 +0200

    Cleanup

--- src/core/__tests__/SongListenedDetector.test.ts
+++ src/core/__tests__/SongListenedDetector.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeEach } from 'vitest';
+import { describe, it, expect, beforeEach } from 'bun:test';
 import { SongListenedDetector } from '../SongListenedDetector';
 import { Player } from '../sources/Player';
 
--- src/core/sources/yt-music/content.ts
+++ src/core/sources/yt-music/content.ts
@@ -1,25 +1,44 @@
-// import { Communicator } from "../../Communicator";
-// import { Player } from "../Player";
-// import { YtMusicParser } from "./YtMusicParser";
+import { ChromeCommunicator } from "../../../engines/chrome/ChromeCommunicator";
+import { Communicator } from "../../Communicator";
+import { logger } from "../../Logger";
+import { YtMusicParser } from "./YtMusicParser";
 
-console.log('content.ts');
+logger.info('content.ts');
+
+const parser = new YtMusicParser();
+const communicator = new ChromeCommunicator();
 
 
-// export function p(communicator: Communicator, parser: YtMusicParser) {
-//   setInterval(() => {
-//     const player = {
-//       hasSong: parser.hasSong(), isPlaying: parser.isPlaying(), song: {
-//         album: parser.album(),
-//         artist: parser.artist(),
-//         coverUrl: parser.coverUrl(),
-//         position: parser.songPosition(),
-//         totalDuration: parser.songTotalDuration(),
-//         title: parser.title()
-//       }
-//     } satisfies Player;
-//     communicator.sendTypedMessage();
-//   }, 100);
-//
-//
-//
-// }
+function sendCurrentPlayer(communicator: Communicator) {
+  try {
+    const player = {
+      hasSong: parser.hasSong(), isPlaying: parser.isPlaying(), song: {
+        artist: parser.artist(),
+        album: parser.album(),
+        coverUrl: parser.coverUrl(),
+        position: parser.songPosition(),
+        totalDuration: parser.songTotalDuration(),
+        title: parser.title()
+      }
+    }
+    logger.debug({ player });
+    if (!player.isPlaying) {
+      return;
+    }
+    communicator.sendTypedMessage({ type: 'PLAYER_CURRENT_STATE', player });
+  }
+  catch (error) {
+    logger.info('got an error in content');
+    logger.info({ error });
+  }
+}
+
+setInterval(() => {
+  logger.debug('keep alive');
+  communicator.sendTypedMessage({ type: 'KEEP_ALIVE' });
+}, 20000)
+
+setInterval(() => {
+  logger.debug('interval');
+  sendCurrentPlayer(communicator);
+}, 1000)
