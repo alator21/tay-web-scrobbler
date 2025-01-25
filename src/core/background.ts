7e617b15af892b9ae5e54d8110df119d9dd41568
alator21 <botazz133@gmail.com>
2025-01-25 15:24:05 +0200

    Improving background script

--- src/core/background.ts
+++ src/core/background.ts
@@ -1,78 +1,102 @@
 import { LastFmAuthenticator } from "./LastFmAuthenticator";
 import { Communicator } from "./Communicator";
-import { getSession, scrobble, updateNowPlaying } from "@alator21/lastfm";
+import { getSession } from "@alator21/lastfm";
 import { Storage } from "./Storage";
+import { SongListenedDetector } from "./SongListenedDetector";
+import { logger } from "./Logger";
+import { SongChangedDetector } from "./SongChangedDetector";
+import { CurrentSongPersistor } from "./CurrentSongPersistor";
 
-export function doStuff(communicator: Communicator, lastFmAuthenticator: LastFmAuthenticator, storage: Storage) {
-  console.log('typed listener');
-  communicator.addTypedListener(async (message, sendResponse) => {
-    console.log('message');
-    console.log(message);
-    switch (message.type) {
-      case 'AUTHENTICATE': {
-        try {
-          const status = await storage.get('last_fm_session');
-          if (status !== undefined) {
-            sendResponse({ type: 'AUTHENTICATE', success: false, error: `You can't login when you are already logged in.` });
-            break;
+export function doStuff(
+  communicator: Communicator,
+  lastFmAuthenticator: LastFmAuthenticator,
+  storage: Storage,
+  songListenedDetector: SongListenedDetector,
+  songChangedDetector: SongChangedDetector,
+  currentSongPersistor: CurrentSongPersistor
+) {
+  try {
+    logger.info('Adding typed listener');
+    communicator.addTypedListener(async (message, sendResponse) => {
+      switch (message.type) {
+        case 'AUTHENTICATE': {
+          try {
+            const status = await storage.get('last_fm_session');
+            if (status !== undefined) {
+              sendResponse({ type: 'AUTHENTICATE', success: false, error: `You can't login when you are already logged in.` });
+              break;
+            }
+            const session = await getLastFmSession(lastFmAuthenticator);
+            storage.set('last_fm_session', { session_key: session.key, user: session.name });
+            sendResponse({ type: 'AUTHENTICATE', success: true, data: { user: session.name } });
+          } catch (error) {
+            sendResponse({ type: 'AUTHENTICATE', success: false, error: `error while authenticating` });
           }
-          const session = await getLastFmSession(lastFmAuthenticator);
-          storage.set('last_fm_session', { session_key: session.key, user: session.name });
-          sendResponse({ type: 'AUTHENTICATE', success: true, data: { user: session.name } });
-        } catch (error) {
-          sendResponse({ type: 'AUTHENTICATE', success: false, error: `error while authenticating` });
+          break;
         }
-        break;
-      }
-      case "LOGOUT": {
-        try {
-          const status = await storage.get('last_fm_session');
-          if (status === undefined) {
-            sendResponse({ type: 'LOGOUT', success: false, error: `You can't logout when you are not logged in.` });
-            break;
+        case "LOGOUT": {
+          try {
+            const status = await storage.get('last_fm_session');
+            if (status === undefined) {
+              sendResponse({ type: 'LOGOUT', success: false, error: `You can't logout when you are not logged in.` });
+              break;
+            }
+            await storage.removeAll();
+            sendResponse({ type: 'LOGOUT', success: true });
+          } catch (error) {
+            sendResponse({ type: 'LOGOUT', success: false, error: `error while logging out` });
           }
-          await storage.removeAll();
-          sendResponse({ type: 'LOGOUT', success: true });
-        } catch (error) {
-          sendResponse({ type: 'LOGOUT', success: false, error: `error while logging out` });
+          break;
         }
-        break;
-      }
-      case "SCROBBLE": {
-        try {
-          const { track, artist, timestamp } = message;
-          await scrobbleSong(storage, artist, timestamp, track);
-          sendResponse({ type: 'SCROBBLE', success: true });
-        } catch (error) {
-          sendResponse({ type: 'SCROBBLE', success: false, error: `error while scrobbling` });
+        case "PLAYER_CURRENT_STATE": {
+          try {
+            const { player } = message;
+            logger.debug(`Current state of player`);
+            logger.debug(player);
+            const { artist, title } = player.song;
+            currentSongPersistor.songTick(player);
+            const songChanged = songChangedDetector.songTick(player);
+            const shouldScrobbleSong = songListenedDetector.songTick(player);
+            if (songChanged) {
+              logger.info(`Song changed. Now playing: ${title} by ${artist}`);
+              // await updateCurrentSong(storage, artist, title);
+              // sendResponse({ type: 'PLAYER_CURRENT_STATE' });
+            }
+            if (shouldScrobbleSong) {
+              logger.info(`Scrobbling song: ${title} by ${artist}`);
+              //     await scrobbleSong(storage, artist, new Date(), title);
+            }
+          } catch (error) {
+            logger.info(error);
+            // sendResponse({ type: 'SCROBBLE', success: false, error: `error while updating current song` });
+          }
+          break;
         }
-        break;
-      }
-      case "UPDATE_NOW_PLAYING": {
-        try {
-          const { track, artist } = message;
-          await updateCurrentSong(storage, artist, track);
-          sendResponse({ type: 'SCROBBLE', success: true });
-        } catch (error) {
-          sendResponse({ type: 'SCROBBLE', success: false, error: `error while updating current song` });
+        case "GET_CURRENT_PLAYER_STATE": {
+          sendResponse({ type: 'GET_CURRENT_PLAYER_STATE', success: true, player: currentSongPersistor.getCurrentPlayer() });
+          break;
         }
-        break;
-      }
-      case 'GET_LAST_FM_AUTH_STATUS': {
-        try {
-          const status = await storage.get('last_fm_session');
-          console.log({ status });
-          sendResponse({ type: 'GET_LAST_FM_AUTH_STATUS', success: true, data: status !== undefined ? { sessionKey: status.session_key, user: status.user } : undefined });
-        } catch (error) {
-          sendResponse({ type: 'GET_LAST_FM_AUTH_STATUS', success: false, error: `error while getting status` });
+        case "KEEP_ALIVE": {
+          sendResponse({ type: 'KEEP_ALIVE' });
+          break;
         }
-        break;
+        case 'GET_LAST_FM_AUTH_STATUS': {
+          try {
+            const status = await storage.get('last_fm_session');
+            logger.debug({ status });
+            sendResponse({ type: 'GET_LAST_FM_AUTH_STATUS', success: true, data: status !== undefined ? { sessionKey: status.session_key, user: status.user } : undefined });
+          } catch (error) {
+            sendResponse({ type: 'GET_LAST_FM_AUTH_STATUS', success: false, error: `error while getting status` });
+          }
+          break;
+        }
       }
-    }
-  });
+    });
+  }
+  catch (error) {
+    logger.info(error);
+  }
 }
-
-
 
 async function getLastFmSession(authenticator: LastFmAuthenticator) {
   const authToken = await authenticator.authenticate();
@@ -80,21 +104,22 @@ async function getLastFmSession(authenticator: LastFmAuthenticator) {
   return session;
 }
 
-async function scrobbleSong(storage: Storage, artist: string, timestamp: Date, track: string) {
-  const sessionKey = await getSessionKeyOrThrow(storage);
-  await scrobble({ sessionKey, artist, timestamp, track });
-}
+// async function scrobbleSong(storage: Storage, artist: string, timestamp: Date, track: string) {
+//   const sessionKey = await getSessionKeyOrThrow(storage);
+//   await scrobble({ sessionKey, artist, timestamp, track });
+// }
+//
+// async function updateCurrentSong(storage: Storage, artist: string, track: string) {
+//   const sessionKey = await getSessionKeyOrThrow(storage);
+//   await updateNowPlaying({ sessionKey, artist, track });
+// }
+//
+//
+// async function getSessionKeyOrThrow(storage: Storage) {
+//   const session = await storage.get('last_fm_session');
+//   if (session === undefined) {
+//     throw new Error('Unauthorized');
+//   }
+//   return session.session_key;
+// }
 
-async function updateCurrentSong(storage: Storage, artist: string, track: string) {
-  const sessionKey = await getSessionKeyOrThrow(storage);
-  await updateNowPlaying({ sessionKey, artist, track });
-}
-
-
-async function getSessionKeyOrThrow(storage: Storage) {
-  const session = await storage.get('last_fm_session');
-  if (session === undefined) {
-    throw new Error('Unauthorized');
-  }
-  return session.session_key;
-}
--- src/engines/chrome/background.ts
+++ src/engines/chrome/background.ts
@@ -3,6 +3,10 @@ import { doStuff } from "../../core/background";
 import { ChromeCommunicator } from "./ChromeCommunicator";
 import { ChromeLastFmAuthenticator } from "./ChromeLastFmAuthenticator";
 import { ChromeStorage } from "./ChromeStorage";
+import { SongListenedDetector } from "../../core/SongListenedDetector";
+import { logger } from "../../core/Logger";
+import { SongChangedDetector } from "../../core/SongChangedDetector";
+import { CurrentSongPersistor } from "../../core/CurrentSongPersistor";
 
 const CLIENT_ID = import.meta.env.VITE_LAST_FM_API_KEY;
 const SHARED_SECRET = import.meta.env.VITE_LAST_FM_SHARED_SECRET;
@@ -10,8 +14,11 @@ const SHARED_SECRET = import.meta.env.VITE_LAST_FM_SHARED_SECRET;
 const communicator = new ChromeCommunicator();
 const authenicator = new ChromeLastFmAuthenticator(CLIENT_ID);
 const storage = new ChromeStorage();
+const songListenedDetector = new SongListenedDetector(0.1);
+const songChangedDetector = new SongChangedDetector();
+const currentSongPersistor = new CurrentSongPersistor(5000);
 
 initializeLastFmApi(CLIENT_ID, SHARED_SECRET);
-console.log('chrome/background.ts');
-doStuff(communicator, authenicator, storage);
+logger.info('chrome/background.ts');
+doStuff(communicator, authenicator, storage, songListenedDetector, songChangedDetector, currentSongPersistor);
 
