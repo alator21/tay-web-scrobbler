b8e42aa529d5039a1d2ac3e648c6175a36ce1979
alator21 <botazz133@gmail.com>
2025-01-25 20:29:05 +0200

    Add link to options page

--- src/core/ui/LoggedIn.tsx
+++ src/core/ui/LoggedIn.tsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from "react";
 import { Communicator } from "../Communicator";
 import { Player } from "../sources/Player";
 import { logger } from "../Logger";
+import { Cog6ToothIcon } from "@heroicons/react/24/solid"
 
 type LoggedInProps = { communicator: Communicator, reloadStateFn: () => void, user: string };
 export function LoggedIn({ communicator, reloadStateFn, user }: LoggedInProps) {
   const [player, setPlayer] = useState<Player | undefined>();
+  const [optionsUrl, setOptionsUrl] = useState<string | undefined>();
   useEffect(() => {
     getPlayer();
+    fetchOptionsUrl();
   }, [])
 
   async function getPlayer() {
@@ -24,6 +27,11 @@ export function LoggedIn({ communicator, reloadStateFn, user }: LoggedInProps) {
     setPlayer(player);
   }
 
+  async function fetchOptionsUrl() {
+    const response = await communicator.sendTypedMessage({ type: 'GET_OPTIONS_PAGE_URL' });
+    setOptionsUrl(response.url);
+  }
+
 
   return (
     <div className="space-y-4">
@@ -34,15 +42,26 @@ export function LoggedIn({ communicator, reloadStateFn, user }: LoggedInProps) {
           </div>
           <span className="text-lg font-medium">{user}</span>
         </div>
-        <button
-          onClick={async () => {
-            await communicator.sendTypedMessage({ type: 'LOGOUT' });
-            reloadStateFn();
-          }}
-          className="py-1 px-3 bg-red-600 hover:bg-red-700 transition-colors duration-200 rounded-md text-sm font-medium focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
-        >
-          Logout
-        </button>
+        <div className="flex items-center space-x-2">
+          {optionsUrl !== undefined && (<button
+            onClick={() => {
+              window.open(optionsUrl);
+            }}
+            className="p-1 bg-gray-700 rounded-full hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
+            title="Options"
+          >
+            <Cog6ToothIcon className="h-5 w-5 text-gray-300" />
+          </button>)}
+          <button
+            onClick={async () => {
+              await communicator.sendTypedMessage({ type: 'LOGOUT' });
+              reloadStateFn();
+            }}
+            className="py-1 px-3 bg-red-600 hover:bg-red-700 transition-colors duration-200 rounded-md text-sm font-medium focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
+          >
+            Logout
+          </button>
+        </div>
       </div>
       {player !== undefined ? <div className="flex items-start space-x-3">
         <img
